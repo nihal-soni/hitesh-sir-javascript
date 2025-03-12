3a1c07c762dfca5e5395590b86c8a353b9cc70c9
Nihal soni <nihalsoni0523@gmail.com>
2025-03-12 21:48:32 +0530

    learned about Dom manipulation

--- 05_Iteration/ForEach.js
+++ 05_Iteration/ForEach.js
@@ -1,49 +1,66 @@
-const { ModuleResolutionKind } = require("typescript");
 
-const coding = ["Javascript", "python" , "C++" , "Ruby", "Java"]
+
+const coding = ["Javascript", "python", "C++", "Ruby", "Java"];
 
 // coding.forEach( function (item) {
 //     console.log(item);
-    
+
 // })
 
-// USIng arrow frunction 
+// USIng arrow frunction
 
 // coding.forEach( (item) => {
 //     console.log(item);
-    
+
 // })
 
+coding.forEach((item, index, arr) => {
+  // console.log(item, index, arr);
+});
+
+// lets take another exmaple
+
+const myCoding = [
+  {
+    languageName: "Javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "Python",
+    languageFileName: "Py",
+  },
+  {
+    languageName: "Java",
+    languageFileName: "java",
+  },
+];
+
+myCoding.forEach((item) => {
+  // console.log(item.languageName);
+});
 
 
-coding.forEach ( (item, index, arr) => {
-    console.log(item, index, arr);
-    
-})
-
-// lets take another exmaple 
-
-const myCoding  = [
-    {
-        languageName: "Javascript",
-        languageFileName: "js"
-    },
-    {
-        languageName: "Python",
-        languageFileName: "Py"
-    },
-    {
-        languageName: "Java",
-        languageFileName: "java"
-    }
+const mycoding1= [
+  {
+    languageName: "C++",
+    languageFileName: 'cpp'
+  },
+  {
+    languageName: "Ruby",
+    languageFileName: 'Ruby'
+  },
+  {
+    languageName: "Java",
+    languageFileName: 'java'
+  },
+  {
+    languageName: "go",
+    languageFileName: 'go'
+  }
 ]
 
-myCoding.forEach( (item) => {
-
-   console.log(item.languageName);
-   
+mycoding1.forEach( (item) =>{
+  console.log(item.languageFileName);
 })
 
-
-
-// this is Mostly used in database 
+// this is Mostly used in database
